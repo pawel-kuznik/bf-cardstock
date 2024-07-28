a81b7926d101973b474a86c57388bc578b7aa88a
Pawel <pawel.kuznik@gmail.com>
2024-07-29 00:11:55 +0200

    Adding FactionField

---
+++ src/components/FactionField/FactionField.tsx
@@ -0,0 +1,15 @@
+
+export interface FactionFieldProps {
+
+};
+
+/**
+ *  A component representing a faction field. The faction would be composed
+ *  from three pieces of information: period, alliance, and nation. This is to make
+ *  it cover all of the Battlefront games and represented factions.
+ * 
+ *  
+ */
+export function FactionField() {
+
+};
